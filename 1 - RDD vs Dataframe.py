8415e2800f8b0f4f86e845d4d71ee59330227158
CodyAustinDavis <CodyAustinDavis>
2023-07-31 19:36:48 +0000

    Update Dataframe example
    TO DO: add udf behavior

--- 1 - RDD vs Dataframe.py
+++ 1 - RDD vs Dataframe.py
@@ -39,20 +39,29 @@ print(spark.read.text("dbfs:/databricks-datasets/sample_logs/*").count())
 
 # COMMAND ----------
 
+# MAGIC %scala 
+# MAGIC
+# MAGIC val rawRDD = spark.sparkContext.textFile("dbfs:/databricks-datasets/sample_logs/*");
+# MAGIC val biggerRDD = spark.sparkContext.union(Seq(rawRDD, rawRDD, rawRDD, rawRDD, rawRDD));
+# MAGIC
+
+# COMMAND ----------
+
 # DBTITLE 1,Scala RDD standard functions
 # MAGIC %scala 
 # MAGIC import org.apache.spark.sql.types._;
 # MAGIC import org.apache.spark.sql.functions._;
 # MAGIC
-# MAGIC
+# MAGIC // duplicate 10 times to make it a little bigger
 # MAGIC val rawRDD = spark.sparkContext.textFile("dbfs:/databricks-datasets/sample_logs/*");
+# MAGIC val biggerRDD = spark.sparkContext.union(Seq(rawRDD, rawRDD, rawRDD, rawRDD, rawRDD, rawRDD, rawRDD, rawRDD, rawRDD, rawRDD));
 # MAGIC
 # MAGIC // define schema
 # MAGIC case class LogData(ip: String, mac: String, userId: String, timestampRaw:String, timezoneRow: String, apiCall: String, status: Int, code: Int)
 # MAGIC
 # MAGIC // Load dataset and apply the schema / clean rows
 # MAGIC
-# MAGIC val logDataRDD = rawRDD.map( line => {
+# MAGIC val logDataRDD = biggerRDD.map( line => {
 # MAGIC
 # MAGIC   val fields = line.split(" (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
 # MAGIC
@@ -73,7 +82,7 @@ print(spark.read.text("dbfs:/databricks-datasets/sample_logs/*").count())
 # MAGIC val ipCountAggregate = ipCountData.reduceByKey(_ + _)
 # MAGIC
 # MAGIC println("Aggregates for API calls by IP: ")
-# MAGIC //ipCountAggregate.collect().foreach(println);
+# MAGIC ipCountAggregate.collect().foreach(println);
 # MAGIC
 # MAGIC // Aggregate #2: Get API call count by ip and status
 # MAGIC
@@ -81,11 +90,25 @@ print(spark.read.text("dbfs:/databricks-datasets/sample_logs/*").count())
 # MAGIC
 # MAGIC val ipStatsCountAggregate = ipStatusData.reduceByKey(_ + _)
 # MAGIC
-# MAGIC println("Aggregates for API calls by IP and STATUS: ")
-# MAGIC //ipStatsCountAggregate.collect().foreach(println);
+# MAGIC // If already exists, need to write more code just to delete files where with dataframes, you can just add .mode("overwrite")
 # MAGIC
-# MAGIC ipCountAggregate.saveAsTextFile("dbfs:/temp/workshop/rdd/scala/ip_count")
-# MAGIC ipStatsCountAggregate.saveAsTextFile("dbfs:/temp/workshop/rdd/scala/ip_status_count")
+# MAGIC try {
+# MAGIC   ipCountAggregate.saveAsTextFile("dbfs:/temp/workshop/rdd/scala/ip_count")
+# MAGIC   ipStatsCountAggregate.saveAsTextFile("dbfs:/temp/workshop/rdd/scala/ip_status_count")
+# MAGIC }
+# MAGIC catch {
+# MAGIC   case _ : Throwable => println("Files already exist, deleting then re-saving")
+# MAGIC }
+# MAGIC finally {
+# MAGIC   val srcPath: String= "dbfs:/temp/workshop/rdd/scala/ip_count"
+# MAGIC   dbutils.fs.rm(srcPath, true)
+# MAGIC   val srcPath2: String= "dbfs:/temp/workshop/rdd/scala/ip_status_count"
+# MAGIC   dbutils.fs.rm(srcPath2, true)
+# MAGIC
+# MAGIC   // Re-save files
+# MAGIC   ipCountAggregate.saveAsTextFile("dbfs:/temp/workshop/rdd/scala/ip_count")
+# MAGIC   ipStatsCountAggregate.saveAsTextFile("dbfs:/temp/workshop/rdd/scala/ip_status_count")
+# MAGIC }
 
 # COMMAND ----------
 
@@ -97,7 +120,9 @@ print(spark.read.text("dbfs:/databricks-datasets/sample_logs/*").count())
 # MAGIC
 # MAGIC val df_1 = spark.read.option("sep", " ").option("quote", "\"").csv("dbfs:/databricks-datasets/sample_logs/");
 # MAGIC
-# MAGIC val df_raw = df_1.toDF("ip", "mac", "user_id", "timestamp_raw", "timezone_raw", "api_call", "status", "code");
+# MAGIC val df_bigger = df_1.union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1);
+# MAGIC
+# MAGIC val df_raw = df_bigger.toDF("ip", "mac", "user_id", "timestamp_raw", "timezone_raw", "api_call", "status", "code");
 # MAGIC
 # MAGIC val df_cleaned = {df_raw.select(col("ip"), 
 # MAGIC                   col("mac"), 
@@ -115,10 +140,12 @@ print(spark.read.text("dbfs:/databricks-datasets/sample_logs/*").count())
 # MAGIC
 # MAGIC val df_agg_by_ip_status = df_cleaned.groupBy(col("ip"), col("status")).agg(count(lit(0)).cast("string").alias("api_calls_by_status"));
 # MAGIC
+# MAGIC println("Aggregates for API calls by IP: ")
+# MAGIC df_agg_by_ip.collect().foreach(println)
 # MAGIC
+# MAGIC // Already simpler and more robust
 # MAGIC df_agg_by_ip.write.mode("overwrite").csv("dbfs:/temp/workshop/df/scala/ip_count")
 # MAGIC df_agg_by_ip_status.write.mode("overwrite").csv("dbfs:/temp/workshop/df/scala/ip_status_count")
-# MAGIC
 
 # COMMAND ----------
 
@@ -127,7 +154,9 @@ from pyspark.sql.functions import *
 
 df_1 = spark.read.option("sep", " ").option("quote", "\"").csv('dbfs:/databricks-datasets/sample_logs/')
 
-df_raw = df_1.toDF(*["ip", "mac", "user_id", "timestamp_raw", "timezone_raw", "api_call", "status", "code"])
+df_bigger = df_1.union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1).union(df_1)
+
+df_raw = df_bigger.toDF(*["ip", "mac", "user_id", "timestamp_raw", "timezone_raw", "api_call", "status", "code"])
 
 df_cleaned = (df_raw
           .select(col("ip"), 
@@ -138,7 +167,7 @@ df_cleaned = (df_raw
                   "api_call", 
                   col("status").cast("integer"), 
                   col("code").cast("integer")
-                  )             
+            )             
 )
 
 df_agg_by_ip_python = df_cleaned.groupBy(col("ip")).agg(count(lit(0)).alias("api_calls"))
@@ -146,6 +175,8 @@ df_agg_by_ip_python = df_cleaned.groupBy(col("ip")).agg(count(lit(0)).alias("api
 
 df_agg_by_ip_status_python = df_cleaned.groupBy(col("ip"), col("status")).agg(count(lit(0)).alias("api_calls_by_status"))
 
+print("Aggregates for API calls by IP: ")
+print(df_agg_by_ip_python.collect())
 
 df_agg_by_ip_python.write.mode("overwrite").csv("dbfs:/temp/workshop/df/python/ip_count")
 df_agg_by_ip_status_python.write.mode("overwrite").csv("dbfs:/temp/workshop/df/python/ip_status_count")
